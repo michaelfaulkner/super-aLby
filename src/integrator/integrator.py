c6a64cda845c34dfb412b61adc1d5d968457d1a5
Michael Faulkner <michael.faulkner@bristol.ac.uk>
2020-09-17 17:56:26 +0100

    added markov_chain.py module (with eponymous class) and added its code; also moved step_size and number_of_integration_steps from __init__() of Integrator as both vary within MarkovChain.run()

--- src/integrator/integrator.py
+++ src/integrator/integrator.py
@@ -10,8 +10,7 @@ class Integrator(metaclass=ABCMeta):
     A general integrator class provides the get_flow() function.
     """
 
-    def __init__(self, kinetic_energy_instance, potential_instance, step_size=1, number_of_integration_steps=10,
-                 **kwargs):
+    def __init__(self, kinetic_energy_instance, potential_instance, **kwargs):
         """
         The constructor of the Integrator class.
 
@@ -24,10 +23,6 @@ class Integrator(metaclass=ABCMeta):
             instance of KineticEnergy class.
         potential_instance : instance of Python class
             instance of Potential class.
-        step_size : int, optional
-            step size of numerical integration.
-        number_of_integration_steps : int, optional
-            number of  numerical integration steps between initial and candidate configurations.
         kwargs : Any
             Additional kwargs which are passed to the __init__ method of the next class in the MRO.
 
@@ -37,31 +32,27 @@ class Integrator(metaclass=ABCMeta):
             If the step_size equals 0.
             If the number_of_integration_steps equals 0.
         """
-        if step_size == 0:
-            raise ValueError("Give a value not equal to 0.0 as the step size of the numerical integrator {0}.".format(
-                self.__class__.__name__))
-        if number_of_integration_steps == 0:
-            raise ValueError("Give a value not equal to 0.0 as the number of numerical integration steps {0}.".format(
-                self.__class__.__name__))
         self._kinetic_energy_instance = kinetic_energy_instance
         self._potential_instance = potential_instance
-        self._step_size = step_size
-        self._number_of_integration_steps = number_of_integration_steps
         super().__init__(**kwargs)
 
     @abstractmethod
-    def flow(self, support_variable, momentum, charges=None):
+    def flow(self, momentum, support_variable, number_of_integration_steps, step_size, charges=None):
         """
         Return the Hamiltonian / (super-)relativistic flow between times
             t_0 and t_0 + step_size * number_of_integration_steps.
 
         Parameters
         ----------
+        momentum : numpy_array
+            The momentum associated with each support_variable.
         support_variable : numpy_array
             For soft-matter models, one or many particle-particle separation vectors {r_ij}; for Bayesian models, the
             parameter value; for the Ginzburg-Landau potential on a lattice, the entire array of superconducting phase.
-        momentum : numpy_array
-            The momentum associated with each support_variable.
+        number_of_integration_steps : int, optional
+            number of  numerical integration steps between initial and candidate configurations.
+        step_size : int, optional
+            step size of numerical integration.
         charges : optional
             All the charges needed to calculate the potential and its gradient.
 
--- src/integrator/leapfrog_integrator.py
+++ src/integrator/leapfrog_integrator.py
@@ -8,7 +8,7 @@ class LeapfrogIntegrator(Integrator):
     This class implements the leapfrog numerical integrator.
     """
 
-    def __init__(self, kinetic_energy_instance, potential_instance, step_size=1, number_of_integration_steps=10):
+    def __init__(self, kinetic_energy_instance, potential_instance, number_of_integration_steps=10):
         """
         The constructor of the LeapfrogIntegrator class.
 
@@ -18,26 +18,25 @@ class LeapfrogIntegrator(Integrator):
             instance of KineticEnergy class.
         potential_instance : instance of Python class
             instance of Potential class.
-        step_size : int, optional
-            step size of numerical integration.
-        number_of_integration_steps : int, optional
-            number of  numerical integration steps between initial and candidate configurations.
         """
-        super().__init__(kinetic_energy_instance=kinetic_energy_instance, potential_instance=potential_instance,
-                         step_size=step_size, number_of_integration_steps=number_of_integration_steps)
+        super().__init__(kinetic_energy_instance=kinetic_energy_instance, potential_instance=potential_instance)
 
-    def flow(self, support_variable, momentum, charges=None):
+    def flow(self, momentum, support_variable, number_of_integration_steps, step_size, charges=None):
         """
         Return the Hamiltonian / (super-)relativistic flow between times
             t_0 and t_0 + step_size * number_of_integration_steps.
 
         Parameters
         ----------
+        momentum : numpy_array
+            The momentum associated with each support_variable.
         support_variable : numpy_array
             For soft-matter models, one or many particle-particle separation vectors {r_ij}; for Bayesian models, the
             parameter value; for the Ginzburg-Landau potential on a lattice, the entire array of superconducting phase.
-        momentum : numpy_array
-            The momentum associated with each support_variable.
+        number_of_integration_steps : int, optional
+            number of  numerical integration steps between initial and candidate configurations.
+        step_size : int, optional
+            step size of numerical integration.
         charges : optional
             All the charges needed to calculate the potential and its gradient.
 
@@ -46,16 +45,16 @@ class LeapfrogIntegrator(Integrator):
         numpy_array
             The flow.
         """
-        support_variable_flow = np.empty((len(support_variable), self._number_of_integration_steps))
-        momentum_flow = np.empty((len(momentum), self._number_of_integration_steps))
+        support_variable_flow = np.empty((len(support_variable), number_of_integration_steps))
+        momentum_flow = np.empty((len(momentum), number_of_integration_steps))
         support_variable_flow[:, 0] = support_variable
         momentum_flow[:, 0] = momentum
-        for i in range(self._number_of_integration_steps):
-            intermediate_momentum = (momentum_flow[:, i] - 0.5 * self._step_size *
+        for i in range(number_of_integration_steps):
+            intermediate_momentum = (momentum_flow[:, i] - 0.5 * step_size *
                                      self._potential_instance.gradient(support_variable_flow[:, i], charges=charges))
-            support_variable_flow[:, i + 1] = (support_variable_flow[:, i] + self._step_size *
+            support_variable_flow[:, i + 1] = (support_variable_flow[:, i] + step_size *
                                                self._kinetic_energy_instance.gradient(intermediate_momentum))
-            momentum_flow[:, i + 1] = (intermediate_momentum - 0.5 * self._step_size *
+            momentum_flow[:, i + 1] = (intermediate_momentum - 0.5 * step_size *
                                        self._potential_instance.gradient(support_variable_flow[:, i + 1],
                                                                          charges=charges))
         return np.vstack((support_variable_flow, momentum_flow))
