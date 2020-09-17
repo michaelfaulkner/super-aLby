dfcb681203fbb41ce8555a591b72995d303ba415
Michael Faulkner <michael.faulkner@bristol.ac.uk>
2020-09-17 13:59:12 +0100

    __pos_x_translation(self, psi) --> _pos_x_translation(self, support_variable), etc

--- src/potential/ginzburg_landau_potential.py
+++ src/potential/ginzburg_landau_potential.py
@@ -54,9 +54,9 @@ class GinzburgLandauPotential(Potential):
             The gradient.
         """
         return (self._one_minus_beta * support_variable - self._beta_dot_alpha * (
-                self.__pos_x_translation(support_variable) + self.__neg_x_translation(support_variable) +
-                self.__pos_y_translation(support_variable) + self.__neg_y_translation(support_variable) +
-                self.__pos_z_translation(support_variable) + self.__neg_z_translation(support_variable) -
+                self._pos_x_translation(support_variable) + self._neg_x_translation(support_variable) +
+                self._pos_y_translation(support_variable) + self._neg_y_translation(support_variable) +
+                self._pos_z_translation(support_variable) + self._neg_z_translation(support_variable) -
                 6 * support_variable) + self._beta_dot_lambda * support_variable ** 3)
 
     def potential(self, support_variable, charges=None):
@@ -79,53 +79,53 @@ class GinzburgLandauPotential(Potential):
         """
         return np.sum(
             0.5 * self._one_minus_beta * support_variable ** 2 + 0.5 * self._beta_dot_alpha * (
-                    (self.__pos_x_translation(support_variable) - support_variable) ** 2 +
-                    (self.__pos_y_translation(support_variable) - support_variable) ** 2 +
-                    (self.__pos_z_translation(support_variable) - support_variable) ** 2) +
+                    (self._pos_x_translation(support_variable) - support_variable) ** 2 +
+                    (self._pos_y_translation(support_variable) - support_variable) ** 2 +
+                    (self._pos_z_translation(support_variable) - support_variable) ** 2) +
             0.25 * self._beta_dot_lambda * support_variable ** 4)
 
-    def __pos_x_translation(self, psi):
-        a = np.reshape(psi, (self._lattice_length, self._lattice_length, self._lattice_length))  # reshapes to a Len*Len*Len matrix
+    def _pos_x_translation(self, support_variable):
+        a = np.reshape(support_variable, (self._lattice_length, self._lattice_length, self._lattice_length))  # reshapes to a Len*Len*Len matrix
         b = np.pad(a, (0, 1), mode='wrap')  # copies the 0th entry at each matrix level to (Len+1)th entry
         c = np.delete(b, self._lattice_length, 0)  # deletes the (Len)th entry at the highest matrix level
         d = np.delete(c, self._lattice_length, 1)  # deletes the (Len)th entry at the second-highest matrix level
         e = np.delete(d, 0, 2)  # deletes the 0th entry at the lowest matrix level
         return np.reshape(e, self._lattice_volume)  # reshapes to an Len**3-dim vector
 
-    def __pos_y_translation(self, psi):
-        a = np.reshape(psi, (self._lattice_length, self._lattice_length, self._lattice_length))
+    def _pos_y_translation(self, support_variable):
+        a = np.reshape(support_variable, (self._lattice_length, self._lattice_length, self._lattice_length))
         b = np.pad(a, (0, 1), mode='wrap')
         c = np.delete(b, self._lattice_length, 0)
         d = np.delete(c, 0, 1)
         e = np.delete(d, self._lattice_length, 2)
         return np.reshape(e, self._lattice_volume)
 
-    def __pos_z_translation(self, psi):
-        a = np.reshape(psi, (self._lattice_length, self._lattice_length, self._lattice_length))
+    def _pos_z_translation(self, support_variable):
+        a = np.reshape(support_variable, (self._lattice_length, self._lattice_length, self._lattice_length))
         b = np.pad(a, (0, 1), mode='wrap')
         c = np.delete(b, 0, 0)
         d = np.delete(c, self._lattice_length, 1)
         e = np.delete(d, self._lattice_length, 2)
         return np.reshape(e, self._lattice_volume)
 
-    def __neg_x_translation(self, psi):
-        a = np.reshape(psi, (self._lattice_length, self._lattice_length, self._lattice_length))
+    def _neg_x_translation(self, support_variable):
+        a = np.reshape(support_variable, (self._lattice_length, self._lattice_length, self._lattice_length))
         b = np.pad(a, (1, 0), mode='wrap')
         c = np.delete(b, 0, 0)
         d = np.delete(c, 0, 1)
         e = np.delete(d, self._lattice_length, 2)
         return np.reshape(e, self._lattice_volume)
 
-    def __neg_y_translation(self, psi):
-        a = np.reshape(psi, (self._lattice_length, self._lattice_length, self._lattice_length))
+    def _neg_y_translation(self, support_variable):
+        a = np.reshape(support_variable, (self._lattice_length, self._lattice_length, self._lattice_length))
         b = np.pad(a, (1, 0), mode='wrap')
         c = np.delete(b, 0, 0)
         d = np.delete(c, self._lattice_length, 1)
         e = np.delete(d, 0, 2)
         return np.reshape(e, self._lattice_volume)
 
-    def __neg_z_translation(self, psi):
-        a = np.reshape(psi, (self._lattice_length, self._lattice_length, self._lattice_length))
+    def _neg_z_translation(self, support_variable):
+        a = np.reshape(support_variable, (self._lattice_length, self._lattice_length, self._lattice_length))
         b = np.pad(a, (1, 0), mode='wrap')
         c = np.delete(b, self._lattice_length, 0)
         d = np.delete(c, 0, 1)
@@ -133,9 +133,9 @@ class GinzburgLandauPotential(Potential):
         return np.reshape(e, self._lattice_volume)
 
     # todo integrer __reshape_and_pad() dans les fonctions ci-dessous
-    def __reshape_and_pad(self, psi, positive_translation):
+    def _reshape_and_pad(self, support_variable, positive_translation):
         # reshape to a self.lattice_length * self.lattice_length * self.lattice_length matrix
-        a = np.reshape(psi, (self._lattice_length, self._lattice_length, self._lattice_length))
+        a = np.reshape(support_variable, (self._lattice_length, self._lattice_length, self._lattice_length))
         # copies the 0th entry at each matrix level to (self.lattice_length + 1)th entry
         if positive_translation:
             return np.pad(a, (0, 1), mode='wrap')
