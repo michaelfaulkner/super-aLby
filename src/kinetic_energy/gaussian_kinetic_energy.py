Moved power from parent KineticEnergy class to inherited classes where it's used; also fixed some bugs in some kinetic_energy() and gradient() expressions

src/kinetic_energy/gaussian_kinetic_energy.py
@@ -8,16 +8,11 @@ class GaussianKineticEnergy(KineticEnergy):
     This class implements the Gaussian kinetic energy K = sum(p[i] ** 2 / 2)
     """
 
-    def __init__(self, power=2):
+    def __init__(self):
         """
         The constructor of the GaussianKineticEnergy class.
-
-        Parameters
-        ----------
-        power : int
-            The power to which each momentum component is raised.
         """
-        super().__init__(power=power)
+        super().__init__()
 
     def gradient(self, momentum):
         """
@@ -49,7 +44,7 @@ class GaussianKineticEnergy(KineticEnergy):
         float
             The kinetic energy.
         """
-        return 0.5 * np.sum(np.absolute(momentum ** 2))
+        return 0.5 * np.sum(momentum ** 2)
 
     def momentum_observation(self, momentum):
         """

src/kinetic_energy/generalised_power_kinetic_energy.py
@@ -22,8 +22,19 @@ class GeneralisedPowerKineticEnergy(KineticEnergy):
             The power to which each momentum component is raised. For potentials with leading order term |x|^a, the
             optimal choice that ensures robust dynamics is given by power = 1 + 1 / (a - 1) for a >= 2 and
             power = 1 + 1 / (a + 1) for a <= -1.
+
+        Raises
+        ------
+        base.exceptions.ValueError
+            If the power equals 0.
         """
-        super().__init__(power=power)
+        if power == 0:
+            raise ValueError("Give a value not equal to 0 as the power associated with the kinetic energy {0}.".format(
+                self.__class__.__name__))
+        self._one_over_power = 1 / power
+        self._power = power
+        self._power_minus_two = power - 2
+        super().__init__()
 
     def gradient(self, momentum):
         """
@@ -39,7 +50,7 @@ class GeneralisedPowerKineticEnergy(KineticEnergy):
         numpy array
             The gradient of the kinetic energy.
         """
-        return momentum * np.absolute(momentum) ** (self._power - 2)
+        return momentum * np.absolute(momentum) ** self._power_minus_two
 
     def kinetic_energy(self, momentum):
         """
@@ -55,7 +66,7 @@ class GeneralisedPowerKineticEnergy(KineticEnergy):
         float
             The kinetic energy.
         """
-        return self._one_over_power * np.sum(np.absolute(momentum ** self._power))
+        return self._one_over_power * np.sum(np.absolute(momentum) ** self._power)
 
     def momentum_observation(self, momentum):
         """

src/kinetic_energy/standard_relativistic_kinetic_energy.py
@@ -8,7 +8,7 @@ class StandardRelativisticKineticEnergy(RelativisticKineticEnergy):
     This class implements the relativistic kinetic energy K = sum((1 + gamma^(-1) p[i] ** 2) ** (1 / 2))
     """
 
-    def __init__(self, gamma=1.0, power=2):
+    def __init__(self, gamma=1.0):
         """
         The constructor of the RelativisticKineticEnergy class.
 
@@ -17,10 +17,8 @@ class StandardRelativisticKineticEnergy(RelativisticKineticEnergy):
         gamma : float
             The tuning parameter that controls the momentum values near which the kinetic energy transforms from
             Gaussian to generalised-power behaviour.
-        power : int
-            Twice the power to which each momentum-dependent part of the relativistic kinetic energy is raised.
         """
-        super().__init__(gamma=gamma, power=power)
+        super().__init__(gamma=gamma)
 
     def gradient(self, momentum):
         """
@@ -36,7 +34,7 @@ class StandardRelativisticKineticEnergy(RelativisticKineticEnergy):
         numpy array
             The gradient of the kinetic energy.
         """
-        return self._one_over_gamma * momentum * (1 + self._one_over_gamma * momentum ** 2)
+        return self._one_over_gamma * momentum * (1 + self._one_over_gamma * momentum ** 2) ** (- 0.5)
 
     def kinetic_energy(self, momentum):
         """
@@ -52,4 +50,4 @@ class StandardRelativisticKineticEnergy(RelativisticKineticEnergy):
         float
             The kinetic energy.
         """
-        return self._one_over_power * np.sum((1 + self._one_over_gamma * momentum ** 2) ** 0.5)
+        return np.sum((1 + self._one_over_gamma * momentum ** 2) ** 0.5)