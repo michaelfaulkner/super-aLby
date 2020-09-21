aaca7c7dfa19a9ef88f2bb5183dd576d64dd7b0f
Michael Faulkner <michael.faulkner@bristol.ac.uk>
2020-09-21 15:43:41 +0100

    delta changes to run.py script

--- src/run.py
+++ src/run.py
@@ -9,9 +9,11 @@ kinetic_energy_instance = kinetic_energy.gaussian_kinetic_energy.GaussianKinetic
 integrator_instance = integrator.leapfrog_integrator.LeapfrogIntegrator(kinetic_energy_instance, potential_instance)
 markov_chain_instance = markov_chain.MarkovChain(integrator_instance, kinetic_energy_instance, potential_instance,
                                                  initial_step_size=1.0, max_number_of_integration_steps=10,
-                                                 number_of_equilibration_iterations=100, number_of_observations=1100,
+                                                 number_of_equilibration_iterations=10000,
+                                                 number_of_observations=10000,
                                                  step_size_adaptor_is_on=True, use_metropolis_accept_reject=True,
                                                  randomise_number_of_integration_steps=False)
-support_variable = np.zeros(10)
-momentum_sample, support_variable_sample, adapted_step_size, acceptance_rate, number_of_numerical_divergences = (
-    markov_chain_instance.run(support_variable, charges=None))
+support_variable = np.zeros(1000)
+(momentum_sample, support_variable_sample, adapted_step_size, acceptance_rate,
+ number_of_numerical_divergences_during_equilibration,
+ number_of_numerical_divergences_during_equilibrated_process) = markov_chain_instance.run(support_variable)
