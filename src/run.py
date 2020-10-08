0de49f232a35986627800911bf7280ff67d0ab60
Michael Faulkner <michael.faulkner@bristol.ac.uk>
2020-10-08 16:51:48 +0100

    exported config_file, parsing and logging fns to config_file module; changed config_file --> config_file_parsing_and_logging; fixed bug in reading values in ini files - no longer strings

--- src/run.py
+++ src/run.py
@@ -1,8 +1,5 @@
 """Executable script which runs the super-relativistic-mc application based on an input configuration file. This script
     and the base package are both taken from the JeLLyFysh application, on which two of the 2-2authors worked."""
-from argparse import ArgumentParser, Namespace
-from configparser import ConfigParser
-import logging
 import platform
 import sys
 import time
@@ -10,135 +7,11 @@ from typing import Sequence
 from base import factory
 from base.strings import to_camel_case
 from base.uuid import get_uuid
+from config_file_parsing_and_logging import get_algorithm_config, parse_options, set_up_logging, read_config
 from version import version
 import integrator.leapfrog_integrator
 import markov_chain
 import numpy as np
-
-
-def add_general_parser_arguments(parser: ArgumentParser) -> None:
-    """
-    Add parser arguments to the command line argument parser.
-
-    This method adds the following possible arguments:
-    1. --version, -V: Print the version of the application and exit.
-    2. --verbose, -v: Increase verbosity of logging messages. Multiple -v options increase the verbosity. The maximum
-    is 2.
-    3. --logfile LOGFILE, -l LOGFILE: Specify the logging file.
-    Per default, also the following argument is added:
-    4. --help, -h: Show the help message and exit.
-
-    Parameters
-    ----------
-    parser : argparse.ArgumentParser
-        The argument parser.
-    """
-    parser.add_argument(
-        "-V", "--version", action="version", version="super-relativistic-mc application version " + version)
-    parser.add_argument("-v", "--verbose", action="count",
-                        help="increase verbosity of logging messages "
-                             "(multiple -v options increase the verbosity, the maximum is 2)")
-    parser.add_argument("-l", "--logfile", action="store", help="specify the logging file")
-
-
-def parse_options(args: Sequence[str]) -> Namespace:
-    """
-    Convert argument strings to objects and assign them as attributes of the argparse namespace. Return the populated
-    namespace.
-
-    The argument strings can for example be sys.argv[1:] in order to parse the command line arguments. The argument
-    parser parses the arguments specified in the add_general_parser_arguments function. This function also adds the
-    configuration file as an obligatory positional argument.
-
-    Parameters
-    ----------
-    args : Sequence[str]
-        The argument strings.
-
-    Returns
-    -------
-    argparse.Namespace
-        The populated argparse namespace.
-    """
-    parser = ArgumentParser()
-    parser.add_argument("config_file", help="specify the path to config.ini file")
-    add_general_parser_arguments(parser)
-    return parser.parse_args(args)
-
-
-def set_up_logging(parsed_arguments: Namespace) -> logging.Logger:
-    """
-    Set up the logging based on the populated argparse namespace.
-
-    The level of the root logger is set based on the number of -v arguments parsed. For zero -v arguments, it is set
-    to logging.WARNING, for one to logging.INFO and for two to logging.DEBUG. If a log file was specified in the
-    parsed arguments, the logging information is written into that file.
-
-    Parameters
-    ----------
-    parsed_arguments : argparse.Namespace
-        The populated argparse namespace.
-
-    Returns
-    -------
-    logging.Logger
-        The initialized root logger.
-    """
-    logger = logging.getLogger("")
-    logger.setLevel(logging.DEBUG)
-
-    if parsed_arguments.verbose is None:
-        logger_level = logging.WARNING
-    elif parsed_arguments.verbose == 1:
-        logger_level = logging.INFO
-    else:
-        logger_level = logging.DEBUG
-
-    if parsed_arguments.logfile is not None:
-        handler = logging.FileHandler(parsed_arguments.logfile)
-    else:
-        handler = logging.StreamHandler()
-    logger.setLevel(logger_level)
-    handler.setLevel(logger_level)
-    formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(name)s: %(message)s")
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
-    # Don't collect information about where calls were made from (slows down PyPy)
-    logging._srcfile = None
-    return logger
-
-
-def read_config(config_file: str) -> ConfigParser:
-    """
-    Parse the configuration file.
-
-    Parameters
-    ----------
-    config_file : str
-        The filename of the configuration file.
-
-    Returns
-    -------
-    configparser.ConfigParser
-        The parsed configuration file.
-
-    Raises
-    ------
-    RuntimeError
-        If the configuration file does not exist.
-    """
-    config = ConfigParser()
-    if not config.read(config_file):
-        raise RuntimeError("Given configuration file does not exist.")
-    return config
-
-
-def get_algorithm_config(config):
-    return (config.get("Algorithm", "initial_step_size"), config.get("Algorithm", "max_number_of_integration_steps"),
-            config.get("Algorithm", "number_of_equilibration_iterations"),
-            config.get("Algorithm", "number_of_observations"), config.get("Algorithm", "step_size_adaptor_is_on"),
-            config.get("Algorithm", "use_metropolis_accept_reject"),
-            config.get("Algorithm", "randomise_number_of_integration_steps"))
 
 
 def print_start_message():
@@ -169,6 +42,9 @@ def main(argv: Sequence[str]) -> None:
 
     logger.info("Setting up the run based on the configuration file {0}.".format(args.config_file))
     config = read_config(args.config_file)
+    print(config)
+    algorithm_config = get_algorithm_config(config)
+    print(algorithm_config)
     kinetic_energy_instance = factory.build_from_config(
         config, to_camel_case(config.get("Hamiltonian", "kinetic_energy")), "kinetic_energy")
     potential_instance = factory.build_from_config(
