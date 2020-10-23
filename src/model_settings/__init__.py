78db666d1af83e1b0e67cc6ebaf1bc90f2a21005
Michael Faulkner <michael.faulkner@bristol.ac.uk>
2020-10-23 15:07:28 +0100

    inverse_temperature --> beta (as it's shorter)

--- src/model_settings/__init__.py
+++ src/model_settings/__init__.py
@@ -4,7 +4,7 @@ import sys
 
 args = parse_options(sys.argv[1:])
 config = read_config(args.config_file)
-inverse_temperature = get_value(config, "ModelSettings", "inverse_temperature")
+beta = get_value(config, "ModelSettings", "beta")
 number_of_particles = get_value(config, "ModelSettings", "number_of_particles")
 dimensionality_of_particle_space = get_value(config, "ModelSettings", "dimensionality_of_particle_space")
 size_of_particle_space = get_value(config, "ModelSettings", "size_of_particle_space")
