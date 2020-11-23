52c5af11ba8e7c2338383fafb8e277e946b381ca
Michael Faulkner <michael.faulkner@bristol.ac.uk>
2020-11-23 16:43:13 +0000

    first attempt at the CoulombSoftMatterPotential class. Not yet tested as we need a new ParticleSeparationSampler class.

--- src/potential/coulomb_soft_matter_potential.py
+++ src/potential/coulomb_soft_matter_potential.py
@@ -2,10 +2,9 @@
 from .soft_matter_potential import SoftMatterPotential
 from base.exceptions import ConfigurationError
 from base.logging import log_init_arguments
-from base.vectors import permutation_3d
-from model_settings import dimensionality_of_particle_space, size_of_particle_space
+from base.vectors import get_corrected_separation, permutation_3d
+from model_settings import dimensionality_of_particle_space, number_of_particles, size_of_particle_space
 import logging
-import math
 import numpy as np
 
 
@@ -16,7 +15,7 @@ class CoulombSoftMatterPotential(SoftMatterPotential):
 
     k is a prefactor, c_i and c_j are the charges of the involved units and r_ij = r_j - r_i is the separation between
     the units. \vec{L} are the sides of the three-dimensional simulation box with periodic boundary conditions.
-    This class is (currently) only implemented for a three-dimensional cube.
+    This class is (currently) only implemented for a three-dimensional cube, and for like charges.
     The conditionally convergent sum can be consistently defined in terms of tin-foil boundary conditions. Then,
     the sum is absolutely convergent. Ewald summation splits the sum up partly in position space and partly in Fourier
     space. The summation has three parameters: the cutoff in Fourier space, the cutoff in position space, and a
@@ -45,8 +44,6 @@ class CoulombSoftMatterPotential(SoftMatterPotential):
         ------
         base.exceptions.ConfigurationError
             If the dimension does not equal three.
-        base.exceptions.ConfigurationError
-            If the hypercubic setting is not initialized.
         base.exceptions.ConfigurationError
             If the cutoff in Fourier space is negative.
         base.exceptions.ConfigurationError
@@ -80,6 +77,8 @@ class CoulombSoftMatterPotential(SoftMatterPotential):
 
         fourier_list = [[[0.0 for _ in range(self._fourier_cutoff + 1)] for _ in
                          range(self._fourier_cutoff + 1)] for _ in range(self._fourier_cutoff + 1)]
+        fourier_list_for_potential = [[[0.0 for _ in range(self._fourier_cutoff + 1)] for _ in
+                                       range(self._fourier_cutoff + 1)] for _ in range(self._fourier_cutoff + 1)]
         for k in range(0, self._fourier_cutoff + 1):
             for j in range(0, self._fourier_cutoff + 1):
                 for i in range(1, self._fourier_cutoff + 1):
@@ -93,12 +92,20 @@ class CoulombSoftMatterPotential(SoftMatterPotential):
                         coefficient = 4.0
 
                     norm_sq = i * i + j * j + k * k
+                    base_fourier_list_component = 2 * coefficient * (
+                            np.exp(- pi_sq * norm_sq / self._alpha_sq / length_sq) / norm_sq / self._system_length)
+                    fourier_list_for_potential[i][j][k] = base_fourier_list_component / np.pi
+                    # todo change the following to 2 * base_fourier_list_component / self._system_length
                     fourier_list[i][j][k] = 4 * i * coefficient * (np.exp(
                         - pi_sq * norm_sq / self._alpha_sq / length_sq) / norm_sq / length_sq)
 
         self._fourier_array = tuple(
             [tuple([tuple(fourier_list[i][j]) for j in range(self._fourier_cutoff + 1)]) for i in
              range(self._fourier_cutoff + 1)])
+
+        self._fourier_array_for_potential = tuple(
+            [tuple([tuple(fourier_list_for_potential[i][j]) for j in range(self._fourier_cutoff + 1)])
+             for i in range(self._fourier_cutoff + 1)])
 
         super().__init__(prefactor=prefactor)
         log_init_arguments(logging.getLogger(__name__).debug, self.__class__.__name__,
@@ -112,15 +119,23 @@ class CoulombSoftMatterPotential(SoftMatterPotential):
         Parameters
         ----------
         positions : numpy.ndarray
-            For soft-matter models, one or many particle-particle separation vectors {r_ij}; in this case, the Bayesian
-            parameter value.
+            The particle-particle separation vectors {r_ij}.
 
         Returns
         -------
         float
             The potential.
         """
-        return positions
+        # todo add functionality for non-like charges
+        potential = 0.0
+        for i in range(number_of_particles):
+            for j in range(i, number_of_particles):
+                separation = get_corrected_separation(positions[i] - positions[j])
+                for direction in range(3):
+                    permuted_separation = permutation_3d(separation, direction)
+                    potential += (self._get_two_particle_position_space_potential(*permuted_separation) +
+                                  self._get_two_particle_fourier_space_potential(*permuted_separation))
+        return potential
 
     def get_gradient(self, positions):
         """
@@ -129,19 +144,92 @@ class CoulombSoftMatterPotential(SoftMatterPotential):
         Parameters
         ----------
         positions : numpy.ndarray
-            For soft-matter models, one or many particle-particle separation vectors {r_ij}; in this case, the Bayesian
-            parameter value.
+            The particle-particle separation vectors {r_ij}.
 
         Returns
         -------
         numpy.ndarray
             The gradient.
         """
-        separation = permutation_3d(separation, direction)
-        return self._get_two_particle_position_space_gradient(*separation) + self._get_two_particle_fourier_space_gradient(*separation)
+        gradient = np.array([np.zeros(3) for _ in range(number_of_particles)])
+        for i in range(number_of_particles):
+            for j in range(i, number_of_particles):
+                separation = get_corrected_separation(positions[i] - positions[j])
+                for direction in range(3):
+                    permuted_separation = permutation_3d(separation, direction)
+                    two_particle_gradient = (self._get_two_particle_position_space_gradient(*permuted_separation) +
+                                             self._get_two_particle_fourier_space_gradient(*permuted_separation))
+                    gradient[i][direction] += two_particle_gradient
+                    gradient[j][direction] -= two_particle_gradient
+        return gradient
+
+    def _get_two_particle_position_space_potential(self, separation_x, separation_y, separation_z):
+        """Returns the position-space part of the Ewald sum of the two-particle potential."""
+        two_particle_position_space_potential = 0.0
+
+        for k in range(- self._position_cutoff, self._position_cutoff + 1):
+            vector_z_sq = (separation_z + k * self._system_length) * (separation_z + k * self._system_length)
+            cutoff_y = int((self._position_cutoff_sq - k * k) ** 0.5)
+            for j in range(- cutoff_y, cutoff_y + 1):
+                vector_y_sq = (separation_y + j * self._system_length) * (separation_y + j * self._system_length)
+                cutoff_x = int((self._position_cutoff_sq - j * j - k * k) ** 0.5)
+                for i in range(- cutoff_x, cutoff_x + 1):
+                    vector_x = separation_x + i * self._system_length
+                    vector_sq = vector_x * vector_x + vector_y_sq + vector_z_sq
+                    vector_norm = vector_sq ** 0.5
+                    two_particle_position_space_potential += np.erfc(self._alpha * vector_norm) / vector_norm
+
+        return two_particle_position_space_potential
+
+    def _get_two_particle_fourier_space_potential(self, separation_x, separation_y, separation_z):
+        """Returns the Fourier-space part of the Ewald sum of the two-particle potential."""
+        two_particle_fourier_space_potential = 0.0
+
+        delta_cos_x = np.cos(self.two_pi_over_length * separation_x)
+        delta_sin_x = np.sin(self.two_pi_over_length * separation_x)
+        delta_cos_y = np.cos(self.two_pi_over_length * separation_y)
+        delta_sin_y = np.sin(self.two_pi_over_length * separation_y)
+        delta_cos_z = np.cos(self.two_pi_over_length * separation_z)
+        delta_sin_z = np.sin(self.two_pi_over_length * separation_z)
+
+        cos_x = delta_cos_x
+        sin_x = delta_sin_x
+        cos_y = 1.0
+        sin_y = 0.0
+        cos_z = 1.0
+        sin_z = 0.0
+
+        for i in range(1, self._fourier_cutoff + 1):
+            cutoff_y = int((self._fourier_cutoff_sq - i * i) ** 0.5)
+            for j in range(0, cutoff_y + 1):
+                cutoff_z = int((self._fourier_cutoff_sq - i * i - j * j) ** 0.5)
+                for k in range(0, cutoff_z + 1):
+                    two_particle_fourier_space_potential += (self._fourier_array_for_potential[i][j][k] *
+                                                             cos_x * cos_y * cos_z)
+
+                    if k != cutoff_z:
+                        store_cos_z = cos_z
+                        cos_z = store_cos_z * delta_cos_z - sin_z * delta_sin_z
+                        sin_z = sin_z * delta_cos_z + store_cos_z * delta_sin_z
+                    elif j != cutoff_y:
+                        store_cos_y = cos_y
+                        cos_y = store_cos_y * delta_cos_y - sin_y * delta_sin_y
+                        sin_y = sin_y * delta_cos_y + store_cos_y * delta_sin_y
+                        cos_z = 1.0
+                        sin_z = 0.0
+                    elif i != self._fourier_cutoff:
+                        store_cos_x = cos_x
+                        cos_x = store_cos_x * delta_cos_x - sin_x * delta_sin_x
+                        sin_x = sin_x * delta_cos_x + store_cos_x * delta_sin_x
+                        cos_y = 1.0
+                        sin_y = 0.0
+                        cos_z = 1.0
+                        sin_z = 0.0
+
+        return two_particle_fourier_space_potential
 
     def _get_two_particle_position_space_gradient(self, separation_x, separation_y, separation_z):
-        """Return the position-space part of the Ewald sum of the two-particle gradient."""
+        """Returns the position-space part of the Ewald sum of the two-particle gradient."""
         two_particle_position_space_gradient = 0.0
 
         for k in range(- self._position_cutoff, self._position_cutoff + 1):
@@ -161,7 +249,7 @@ class CoulombSoftMatterPotential(SoftMatterPotential):
         return two_particle_position_space_gradient
 
     def _get_two_particle_fourier_space_gradient(self, separation_x, separation_y, separation_z):
-        """Return the Fourier-space part of the Ewald sum of the two-particle gradient"""
+        """Returns the Fourier-space part of the Ewald sum of the two-particle gradient."""
         two_particle_fourier_space_gradient = 0.0
 
         delta_cos_x = np.cos(self.two_pi_over_length * separation_x)
