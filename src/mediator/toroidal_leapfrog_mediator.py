50081a4c5a8a9173067b849ad9ac273902c99bbd
Michael Faulkner <michael.faulkner@bristol.ac.uk>
2021-01-18 15:38:39 +0000

    resimplified this part as it introduced a strange bug: candidate_momenta was left unchanged if candidate_momenta -= 0.5 * self._step_size * self._potential.get_gradient(self._positions) is applied just after candidate_momenta = self._momenta; elsewhere candidate_momenta -= self._step_size * self._potential.get_gradient(self._positions) behaves correctly (so the bug could be some memory allocation problem...). The close convergence of the desimplified method must have coincidentally (approximately - convergence wasn't perfect) cancelled out the bug in the Coulomb code.

--- src/mediator/toroidal_leapfrog_mediator.py
+++ src/mediator/toroidal_leapfrog_mediator.py
@@ -106,11 +106,7 @@ class ToroidalLeapfrogMediator(Mediator):
         float
             The potential of the candidate configuration.
         """
-        # todo why does this converge well for two coulomb particles but
-        #  candidate_momenta = self._momenta - 0.5 * self._step_size * self._potential.get_gradient(self._positions)
-        #  in place of the first two lines does not? Same result using LazyToroidalLeapfrogMediator.
-        candidate_momenta = self._momenta
-        candidate_momenta -= 0.5 * self._step_size * self._potential.get_gradient(self._positions)
+        candidate_momenta = self._momenta - 0.5 * self._step_size * self._potential.get_gradient(self._positions)
         candidate_positions = get_shortest_vectors_on_torus(self._positions + self._step_size *
                                                             self._kinetic_energy.get_gradient(candidate_momenta))
         for _ in range(self._number_of_integration_steps - 1):
